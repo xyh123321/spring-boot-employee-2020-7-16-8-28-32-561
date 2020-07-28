16e0ef1bd820a6e7fc78f7ea5a331c4b78b8b7b5
Leewansze <1226186467@qq.com>
2020-07-28 19:27:28 +0800

    [alice] feature: Initialize the employee class

--- src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/entity/Employee.java
@@ -1,4 +1,40 @@
 package com.thoughtworks.springbootemployee.entity;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String gender;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
