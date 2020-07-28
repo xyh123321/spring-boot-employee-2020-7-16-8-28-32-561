d54b74d7a380b49e964c1f1615fc9fb3d075108f
Leewansze <1226186467@qq.com>
2020-07-28 19:46:17 +0800

    [alice] feature: add obtain employee list method

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
+    @Autowired
+    EmployeeServiceImpl employeeServiceImpl;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeServiceImpl.getEmployees();
+    }
 }
--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeServiceImpl.java
@@ -1,8 +1,17 @@
 package com.thoughtworks.springbootemployee.repository;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+    private List<Employee> employees = new ArrayList<>();
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
 }
