37c5c3a905194df9a95be26284d869ed5dd002d2
xyh123321 <376564838@qq.com>
2020-07-29 08:21:11 +0800

    [alice] refactor: Add methods to the EmployeeService interface

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.springbootemployee.service;
 
+import com.thoughtworks.springbootemployee.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+    List<Employee> getEmployees();
+
+    Employee getSpecificEmployee(int id);
+
+    void addEmployees(Employee employee);
+
+    void deleteEmployees(int id);
+
+    List<Employee> getMaleEmployees(String gender);
+
+    void updateEmployees(int id, Employee employee);
+
+    List<Employee> pagingQueryEmployees(int page, int pageSize);
 }
