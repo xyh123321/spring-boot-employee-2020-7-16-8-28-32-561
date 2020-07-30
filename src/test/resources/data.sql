663bcba84236438603f31a22a1d0f01f6c15b9f1
xyh123321 <376564838@qq.com>
2020-07-30 15:23:32 +0800

    [jayden & colin] feature: Set up the test environment

---
+++ src/test/resources/data.sql
@@ -0,0 +1,17 @@
+
+
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS company;
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  age INT NOT NULL,
+  gender VARCHAR(10) DEFAULT NULL,
+  company_id INT
+);
+
+
+CREATE TABLE company (
+  company_id INT AUTO_INCREMENT  PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
