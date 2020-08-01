629597df206f46983918c1f5c680c709bf991a97
xyh123321 <376564838@qq.com>
2020-08-01 13:54:03 +0800

    [jayden] feature: insert some datas to table

---
+++ src/main/resources/db/migration/V20200801135225__insert_data_to_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (name, age, gender,company_id) VALUES
+ ('chengcheng','54','male',1),
+ ('yuexie','82','female',1),
+ ('haifeng','69','female',1),
+ ('ang','18','male',1);
+
+ INSERT INTO company (name) VALUES('tw');
