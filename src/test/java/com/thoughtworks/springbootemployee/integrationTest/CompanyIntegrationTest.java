87ba2e96a6caa625fd2b70a7b91c1f092c2ed75d
xyh123321 <376564838@qq.com>
2020-08-01 15:05:43 +0800

    [jayden] feature: pass should_return_1_company_when_add_a_company

--- src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
@@ -1,15 +1,21 @@
 package com.thoughtworks.springbootemployee.integrationTest;
 
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +35,21 @@ public class CompanyIntegrationTest {
     void should_return_ok_when_get_all_companies() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/companies").param("unpaged", "true"))
                 .andExpect(status().isOk());
+    }
+
+
+    @Test
+    void should_return_1_company_when_add_a_company() throws Exception {
+        String body = "{\n" +
+                "    \"name\": \"tw2242\"\n" +
+                "}";
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body))
+                .andExpect(status().isOk());
+        List<Company> companies = companyRepository.findAll();
+        assertEquals(1,companies.size());
     }
 
 }
