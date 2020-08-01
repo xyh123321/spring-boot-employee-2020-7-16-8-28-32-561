323e553ff1219d0b7fe29a306bd733d1f3add473
xyh123321 <376564838@qq.com>
2020-08-01 14:37:59 +0800

    [jayden] feature: pass should_return_ok_when_get_all_companies

--- src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
@@ -2,11 +2,15 @@ package com.thoughtworks.springbootemployee.integrationTest;
 
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,5 +25,10 @@ public class CompanyIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    void should_return_ok_when_get_all_companies() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies").param("unpaged", "true"))
+                .andExpect(status().isOk());
+    }
 
 }
