1c91b073f3731739e23e6dc86c0e52c860b6b5fc
xyh123321 <376564838@qq.com>
2020-08-01 13:42:54 +0800

    [jayden] feature: rename and  pass should_return_false_when_get_male_employee_given_a_female_employee_in_employee_repo

--- src/test/java/com/thoughtworks/springbootemployee/integrationTest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationTest/EmployeeIntegrationTest.java
@@ -4,25 +4,21 @@ import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-import org.junit.After;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -72,6 +68,7 @@ public class EmployeeIntegrationTest {
 
     @Test
     void should_return_404_when_add_employee() throws Exception {
+        employeeRepository.deleteAll();
         companyRepository.deleteAll();
         String body = " {\n" +
                 "                \"name\": \"chengcheng2222\",\n" +
@@ -124,6 +121,17 @@ public class EmployeeIntegrationTest {
                 .content(body));
         Optional<Employee> updateEmployee = employeeRepository.findById(id);
         assertEquals(10,updateEmployee.get().getAge());
+    }
+
+    @Test
+    void should_return_false_when_get_male_employee_given_a_female_employee_in_employee_repo() throws Exception {
+        Employee employee = new Employee();
+        employee.setName("test");
+        employee.setGender("female");
+        Employee saveEmployee = employeeRepository.save(employee);
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders
+                .get("/employees").param("gender", "male")).andReturn().getResponse();
+        assertFalse(response.getContentAsString().contains("test"));
     }
 
 }
