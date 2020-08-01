2a18583ff05a9967c341ff688b252c0966dd4307
xyh123321 <376564838@qq.com>
2020-08-01 12:14:28 +0800

    [jayden] feature: add employeeNotFoundException to catch exception

--- src/main/java/com/thoughtworks/springbootemployee/exception/GlobalException.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/GlobalException.java
@@ -13,14 +13,20 @@ public class GlobalException {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CompanyNotFoundException.class)
-    void CompanyNotFoundException() {
+    void companyNotFoundException() {
 
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ConstraintViolationException.class)
     public @ResponseBody
-    String ConstraintViolationException(ConstraintViolationException e){
+    String constraintViolationException(ConstraintViolationException e){
         return e.getConstraintViolations().stream().findFirst().get().getMessage();
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    void employeeNotFoundException() {
+
     }
 }
